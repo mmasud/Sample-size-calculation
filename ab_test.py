b01266d3a96fa757ed5e4a30a2f972b6f1c361b4
mmasud <masud.pervez@yahoo.com>
2021-05-28 01:12:17 +0200

    Create ab_test.py

---
+++ ab_test.py
@@ -0,0 +1,15 @@
+from statsmodels.stats.proportion import proportions_ztest, proportion_confint
+control_results = ab_test[ab_test['group'] == 'control']['converted']
+treatment_results = ab_test[ab_test['group'] == 'treatment']['converted']
+n_con = control_results.count()
+n_treat = treatment_results.count()
+successes = [control_results.sum(), treatment_results.sum()]
+nobs = [n_con, n_treat]
+
+z_stat, pval = proportions_ztest(successes, nobs=nobs)
+(lower_con, lower_treat), (upper_con, upper_treat) = proportion_confint(successes, nobs=nobs, alpha=0.05)
+
+print(f'z statistic: {z_stat:.2f}')
+print(f'p-value: {pval:.3f}')
+print(f'ci 95% for control group: [{lower_con:.3f}, {upper_con:.3f}]')
+print(f'ci 95% for treatment group: [{lower_treat:.3f}, {upper_treat:.3f}]')
